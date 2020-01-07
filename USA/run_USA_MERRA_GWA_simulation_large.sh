694245847303da3648d6688074cbcc3ebf703fbc
KatharinaGruber <katharina.gruber8@gmx.at>
2020-01-07 11:50:12 +0100

    add IL

--- USA/run_USA_MERRA_GWA_simulation_large.sh
+++ USA/run_USA_MERRA_GWA_simulation_large.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 N=${1:-2}
-STATES='CA IA KS OK TX'
+STATES='CA IA IL KS OK TX'
 for state in $STATES
 do
    sem --ungroup -j $N --ungroup python WP_simulation_USA_MERRA_GWA_large.py -state $state
