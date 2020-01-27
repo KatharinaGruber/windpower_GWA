082f3651db68b297bdbb98aa627a70bc372f54ab
KatharinaGruber <katharina.gruber8@gmx.at>
2020-01-27 10:25:45 +0100

    add newline at end of file

--- USA/run_old_MERGWA_large.sh
+++ USA/run_old_MERGWA_large.sh
@@ -3,6 +3,6 @@ N=${1:-2}
 STATES='AK AR AZ CA CO CT DE FL HI IA ID IL IN KS MA MD ME MI MN MO MT NC ND NE NH NJ NM NV NY OH OK OR PA PR RI SD TN TX UT VT WA WI WV WY'
 for state in $STATES
 do
-   sem --ungroup -j $N --ungroup python WP_simulation_USA_ERA_GWA.py -state $state
+   sem --ungroup -j $N --ungroup python sim_onestate_old_MERGWA_large.py -state $state
 done
 sem --wait
