8d5174183691927677a5352b3991e383e1152314
KatharinaGruber <katharina.gruber8@gmx.at>
2020-02-20 14:21:14 +0100

    add preparation of reanalsis data BRA

---
+++ BRA/prepare_BRA_ERA5.py
@@ -0,0 +1,44 @@
+# Script for preparing ERA5 reanalysis data:
+# 1. Calculate effective wind speeds from u and v wind speeds in two heights (10 and 100m)
+# 2. Calculate alpha friction coefficient
+
+from paths_bra import era_path
+import glob
+import numpy as np
+import os
+import xarray as xr
+
+files = glob.glob(era_path+'/*.nc')
+files.sort()
+
+if not os.path.isdir(era_path + '/eff_ws'):
+    os.mkdir(era_path + '/eff_ws')
+
+out_files = glob.glob(era_path + '/eff_ws/*')
+
+# 2006 - 2019
+# split into two periods of 7 years each
+for year in [6,13]:
+	i1 = (year - 6)*12
+	i2 = i1 + 7*12
+	wfile = era_path+'/eff_ws/era5_wind_BRA_' + str(2000+year) + '-' + str(2000+year+6) + '.nc'
+	afile = era_path+'/eff_ws/era5_alpha_BRA_' + str(2000+year) + '-' + str(2000+year+6) + '.nc'
+	if wfile not in out_files:
+		print('calculating wind ' + str(2000+year) + '-' + str(2000+year+6))
+		data = xr.open_mfdataset(files[i1:i2], chunks = {'time': 60})
+		wh10 = ((data.u10**2+data.v10**2)**0.5).compute()
+		wh100 = ((data.u100**2+data.v100**2)**0.5).compute()
+		print('saving wind ' + str(2000+year) + '-' + str(2000+year+6))
+		eff_ws = xr.Dataset({'wh10': wh10,
+							 'wh100': wh100})
+						 
+		eff_ws.to_netcdf(wfile)
+		eff_ws.close()
+		del(eff_ws)
+	if afile not in out_files:
+		print('calculating alpha ' + str(2000+year) + '-' + str(2000+year+6))
+		eff_ws = xr.open_dataset(wfile)
+		alpha = (xr.ufuncs.log(eff_ws.wh100/eff_ws.wh10)/np.log(100/10)).compute()
+		print('saving alpha ' + str(2000+year) + '-' + str(2000+year+6))
+		xr.Dataset({'alpha': alpha}).to_netcdf(afile)
+		del(alpha)
---
+++ BRA/prepare_BRA_MERRA2.py
@@ -0,0 +1,44 @@
+# Script for preparing MERRA-2 reanalysis data:
+# 1. Calculate effective wind speeds from u and v wind speeds in two heights (10 and 50m)
+# 2. Calculate alpha friction coefficient
+
+from paths_bra import mer_path
+import glob
+import numpy as np
+import os
+import xarray as xr
+
+files = glob.glob(mer_path+'/*.nc')
+files.sort()
+
+if not os.path.isdir(mer_path + '/eff_ws'):
+    os.mkdir(mer_path + '/eff_ws')
+
+out_files = glob.glob(mer_path + '/eff_ws/*')
+
+# 2006 - 2019
+# split into two periods of 7 years each
+for year in [6,13]:
+	i1 = (year - 6)*12
+	i2 = i1 + 7*12
+	wfile = mer_path+'/eff_ws/merra2_wind_BRA_' + str(2000+year) + '-' + str(2000+year+6) + '.nc'
+	afile = mer_path+'/eff_ws/merra2_alpha_BRA_' + str(2000+year) + '-' + str(2000+year+6) + '.nc'
+	if wfile not in out_files:
+		print('calculating wind ' + str(2000+year) + '-' + str(2000+year+6))
+		data = xr.open_mfdataset(files[i1:i2], chunks = {'time': 60})
+		wh10 = ((data.U10M**2+data.V10M**2)**0.5).compute()
+		wh50 = ((data.U50M**2+data.V50M**2)**0.5).compute()
+		print('saving wind ' + str(2000+year) + '-' + str(2000+year+6))
+		eff_ws = xr.Dataset({'wh10': wh10,
+							 'wh50': wh50})
+						 
+		eff_ws.to_netcdf(wfile)
+		eff_ws.close()
+		del(eff_ws)
+	if afile not in out_files:
+		print('calculating alpha ' + str(2000+year) + '-' + str(2000+year+6))
+		eff_ws = xr.open_dataset(wfile)
+		alpha = (xr.ufuncs.log(eff_ws.wh50/eff_ws.wh10)/np.log(50/(10+data.DISPH))).compute()
+		print('saving alpha ' + str(2000+year) + '-' + str(2000+year+6))
+		xr.Dataset({'alpha': alpha}).to_netcdf(afile)
+		del(alpha)
