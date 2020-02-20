53a122055dd2cd7d7510912382a35fd41851c4cc
KatharinaGruber <katharina.gruber8@gmx.at>
2020-02-20 09:38:39 +0100

    add CO to large simulation USA

--- USA/run_USA_ERA_GWA_simulation_large.sh
+++ USA/run_USA_ERA_GWA_simulation_large.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 N=${1:-2}
-STATES='CA IA IL KS OK TX'
+STATES='CA CO IA IL KS OK TX'
 for state in $STATES
 do
    sem --ungroup -j $N --ungroup python WP_simulation_USA_ERA_GWA_large.py -state $state
