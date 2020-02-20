d7b8e4cb52e1d74fca3d586d6640c26107b76cc2
KatharinaGruber <katharina.gruber8@gmx.at>
2020-02-20 09:33:45 +0100

    add paths BRA

---
+++ BRA/paths_bra.py
@@ -0,0 +1,6 @@
+bra_path = "/data/users/kgruber/other-data/BRA"
+results_path = "/data/users/kgruber/results/BRA"
+mer_path = "/data/users/kgruber/Data/MERRA/BRA"
+era_path = "/data/users/kgruber/Data/era5/BRA"
+
+script_path = "/data/users/kgruber/Skripts/BRA"
