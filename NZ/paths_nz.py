904f395caeafe1c183d3a1e5feb0b94fd973feed
KatharinaGruber <katharina.gruber8@gmx.at>
2020-06-02 13:38:22 +0200

    add NZ paths

---
+++ NZ/paths_nz.py
@@ -0,0 +1,6 @@
+nz_path = "/data/users/kgruber/other-data/NZ"
+results_path = "/data/users/kgruber/results/NZ"
+mer_path = "/data/users/kgruber/Data/MERRA/NZ"
+era_path = "/data/users/kgruber/Data/era5/NZ"
+
+script_path = "/data/users/kgruber/Skripts/NZ"
