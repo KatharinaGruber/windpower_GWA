40ff2a94398df3408552536e36c4b32e0103c958
KatharinaGruber <katharina.gruber8@gmx.at>
2020-06-02 13:56:14 +0200

    add NZ simulation

---
+++ NZ/WP_simulation_NZ_ERA.py
@@ -0,0 +1,50 @@
+import datetime
+import glob
+import math
+import numpy as np
+import os
+import pandas as pd
+import rasterio
+import statsmodels.api as sm
+import time
+import xarray as xr
+
+import sys
+sys.path.append('../')
+
+from functools import reduce
+from scipy.interpolate import interp1d
+
+from utils import power_curve
+from utils import windpower_simulation_era5
+
+
+from dask.diagnostics import ProgressBar
+ProgressBar().register()
+
+from paths_nz import *
+
+
+# Simulate wind power with ERA5
+wind = xr.open_mfdataset(era_path + "/eff_ws/era5_wind_NZ_*.nc", chunks = {'time': 100})
+alpha = xr.open_mfdataset(era_path + "/eff_ws/era5_alpha_NZ_*.nc", chunks = {'time': 100})
+
+# load windpark data
+windparks = pd.read_csv(nz_path + "/windparks_NZ.csv", delimiter=';', parse_dates=['commissioning'])
+# calculate specific power of turbines (in W)
+windparks['sp'] = windparks.turb_cap*10**6/(windparks.d_rotor**2*np.pi/4)
+
+# without GWA
+outfile = results_path + '/windpower_stat_ERA5.nc'
+
+if outfile not in glob.glob(results_path+'/*'):
+	wps = windpower_simulation_era5(wind.wh100,
+                                    alpha.alpha,
+                                    windparks.Height.values,
+                                    windparks.Capacity.values,
+                                    windparks.sp.values,
+                                    windparks.Longitude.values,
+                                    windparks.Latitude.values,
+                                    windparks.commissioning.values)
+	# save as netcdf
+	wps.to_dataset(name='wp').to_netcdf(outfile)
---
+++ NZ/WP_simulation_NZ_ERA_GWA.py
@@ -0,0 +1,68 @@
+import argparse
+import datetime
+import glob
+import math
+import numpy as np
+import os
+import pandas as pd
+import rasterio
+import statsmodels.api as sm
+import time
+import xarray as xr
+
+import sys
+sys.path.append('../')
+
+from functools import reduce
+from scipy.interpolate import interp1d
+from utils import windpower_simulation_era5
+
+from dask.diagnostics import ProgressBar
+ProgressBar().register()
+
+from paths_nz import *
+
+# get GWA version
+parser = argparse.ArgumentParser(description='Insert optionally GWA')
+parser.add_argument('-GWA')
+args = parser.parse_args()
+if(args.GWA == None):
+    GWA = "3"
+else:
+    GWA = args.GWA
+
+if GWA == "2":
+    results_path = results_path + '/results_GWA2'
+    if not os.path.exists(results_path):
+        os.mkdir(results_path)
+
+
+# Simulate wind power with ERA5
+wind = xr.open_mfdataset(era_path + "/eff_ws/era5_wind_NZ_*.nc", chunks = {'time': 46})
+alpha = xr.open_mfdataset(era_path + "/eff_ws/era5_alpha_NZ_*.nc", chunks = {'time': 46})
+
+# load windpark data
+windparks = pd.read_csv(nz_path + "/windparks_NZ.csv", delimiter=';', parse_dates=['commissioning'])
+# calculate specific power of turbines (in W)
+windparks['sp'] = windparks.turb_cap*10**6/(windparks.d_rotor**2*np.pi/4)
+
+# with GWA
+outfile = results_path + '/windpower_??_ERA5_GWA.nc'
+
+if results_path + '/windpower_NZ_ERA5_GWA.nc' not in glob.glob(outfile):
+    print('calculating ERA5 NZ GWA')
+    if GWA == "3":
+        GWA = xr.open_rasterio(nz_path+'/GWA/GWA3_NZ100m.tif')
+    else:
+        GWA = xr.open_dataarray(nz_path+'/GWA/GWA2_NZ100m.nc')
+    wps = windpower_simulation_era5(wind.wh100,
+                                    alpha.alpha,
+                                    windparks.Height.values,
+                                    windparks.Capacity.values,
+                                    windparks.sp.values,
+                                    windparks.Longitude.values,
+                                    windparks.Latitude.values,
+                                    windparks.commissioning.values,
+                                    GWA)
+    # save as netcdf
+    wps.drop(['x','y']).to_dataset(name='wp').to_netcdf(results_path+"/windpower_NZ_ERA5_GWA.nc")
