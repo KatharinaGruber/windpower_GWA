af4b2bdc0c296a5b8fe7d485ce9a6a82e8075904
KatharinaGruber <katharina.gruber8@gmx.at>
2020-06-09 13:39:29 +0200

    add paths for ZAF

---
+++ ZAF/paths_zaf.py
@@ -0,0 +1,6 @@
+zaf_path = "/data/users/kgruber/other-data/ZAF"
+results_path = "/data/users/kgruber/results/ZAF"
+mer_path = "/data/users/kgruber/Data/MERRA/ZAF"
+era_path = "/data/users/kgruber/Data/era5/ZAF"
+
+script_path = "/data/users/kgruber/Skripts/ZAF"
